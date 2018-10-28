2f4b21a23dc94e289e298257a9f13347ac5179b4
Hansraj Das <raj.das.136@gmail.com>
2018-10-28 16:30:02 +0530

    new resume added

--- copy.sh
+++ copy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 cp /home/hansraj/Desktop/resume/*.pdf /home/hansraj/Desktop/Interviews/programs/resume/
-cp /home/hansraj/Desktop/resume/*.json /home/hansraj/Desktop/Interviews/programs/resume/
 cp /home/hansraj/Desktop/resume/*.txt /home/hansraj/Desktop/Interviews/programs/resume/
 
 git status -s
