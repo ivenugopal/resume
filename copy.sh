c38f102f28bd96a731114244d14d23e25e1a174d
Hansraj Das <raj.das.136@gmail.com>
2018-10-28 12:20:59 +0530

    Updating resume

---
+++ copy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp /home/hansraj/Desktop/resume/*.pdf /home/hansraj/Desktop/Interviews/programs/resume/
+cp /home/hansraj/Desktop/resume/*.json /home/hansraj/Desktop/Interviews/programs/resume/
+cp /home/hansraj/Desktop/resume/*.txt /home/hansraj/Desktop/Interviews/programs/resume/
+
+git add .
+git commit -am "Updating resume"
+git push
