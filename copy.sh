7d6555a74c61c011cffd2b0acffac6d3311dd390
Hansraj Das <raj.das.136@gmail.com>
2018-10-28 14:04:14 +0530

    Removed dots from mail id

--- copy.sh
+++ copy.sh
@@ -5,6 +5,7 @@ cp /home/hansraj/Desktop/resume/*.json /home/hansraj/Desktop/Interviews/programs
 cp /home/hansraj/Desktop/resume/*.txt /home/hansraj/Desktop/Interviews/programs/resume/
 
 git status -s
+git diff
 git add .
-git commit -am "Updating resume"
+git commit -am "$1"
 git push
